53ecf4a58515eb022367b8da0aaba15fb35565d0
raghadanees <raghad.anees@hotmail.com>
2020-10-12 21:11:48 +0300

    Updates

---
+++ precache-manifest.3ac30a49fda9f82307f58f7f9b17c276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdca108b02cabfa9a750a1425f09392",
+    "url": "/401-advanced-javascript-raghadanees/resty/index.html"
+  },
+  {
+    "revision": "5284c44c056483d6b2da",
+    "url": "/401-advanced-javascript-raghadanees/resty/static/css/main.d96fe8be.chunk.css"
+  },
+  {
+    "revision": "e8cf487a5bff45db18b7",
+    "url": "/401-advanced-javascript-raghadanees/resty/static/js/2.a54e7450.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/401-advanced-javascript-raghadanees/resty/static/js/2.a54e7450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5284c44c056483d6b2da",
+    "url": "/401-advanced-javascript-raghadanees/resty/static/js/main.62bcc4e8.chunk.js"
+  },
+  {
+    "revision": "f8d596c3c869946ae1ec",
+    "url": "/401-advanced-javascript-raghadanees/resty/static/js/runtime-main.ef167c13.js"
+  }
+]);
